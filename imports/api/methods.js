1679bc7d13296fe5e81beeb6955f7c877d58d3a1
Sven Harder <info@i-modich.de>
2016-10-03 12:22:05 +0200

    End Video 10

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,8 +1,11 @@
 import {Meteor} from 'meteor/meteor';
+import {check} from 'meteor/check';
 import {Items} from './items';
 
 Meteor.methods({
 	createNewItem(itemOne, itemTwo) {
+		check(itemOne, String);
+		check(itemTwo, String);
 		Items.insert({
 			itemOne: {
 				text: itemOne,
@@ -13,5 +16,18 @@ Meteor.methods({
 				value: 0
 			}
 		});
+	},
+	voteOnItem(which, id) {
+		check(which, String);
+		check(id, String);
+		if (which === 'itemOne') {
+			Items.update(id, {
+				$inc: {'itemOne.value': 1}
+			});
+		} else {
+			Items.update(id, {
+				$inc: {'itemTwo.value': 1}
+			});	
+		}
 	}
 });
